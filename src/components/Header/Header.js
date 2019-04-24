5ef633c6be9fcad71461b983206a3e1868c1d0a6
d8musica <d8musica@gmail.com>
2019-04-23 20:57:16 -0500

    primer avance, sin funcionar react materialize

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Navbar,NavItem} from 'react-materialize' //porque desde el yarn lo instalé
+import {Link} from "react-router-dom"
+import './Header.css' //depende de la arquitectura del proyecto la forma de llamar cosas
+import Logo from './img1.png'
+
+
+const Header = () => {
+
+
+    return (
+      <div>
+      <Navbar brand=<img src={Logo} alt="" className='neptune_logo'></img> left className='navbar-css'>
+        <div className='link-container'>
+          <Link to="/about">Quienes somos</Link>
+          <Link to="/services">Nuestros servicios</Link>
+          <Link to="/contactus">Contáctanos</Link>
+        </div>
+      </Navbar>
+
+      </div>
+    );
+
+
+}
+
+export default Header;
