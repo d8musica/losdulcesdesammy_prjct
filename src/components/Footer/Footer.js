6e9e696a94596d4fb646a0b982df83189b351cf5
d8musica <d8musica@gmail.com>
2019-05-06 15:00:02 -0500

    iframe listo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -23,10 +23,10 @@ const Home = () => {
 
             </div>
             <div className="col l3 s12">
-              <h5 className="white-text quesha-font">Contáctanos</h5>
+              <h5 className="white-text quesha-font center-align">Contáctanos</h5>
               <ul>
 
-                <li className = "social-container">
+                <li className = "social-container center-align">
                   <a className="waves-effect btn-floating social center-align transparent" href="https://www.instagram.com/losdulcesdesamy/">
                     <FontAwesomeIcon className="social" icon={faInstagram} size="2x"/>
                   </a>
@@ -38,14 +38,14 @@ const Home = () => {
                   </a>
                 </li>
 
-                <li><a className="white-text avenir-font" href="#!">Email</a></li>
-                <li><a className="white-text avenir-font" href="#!">Teléfono</a></li>
+                <li className="white-text avenir-font">Email</li>
+                <li className="white-text avenir-font">(+57) 305 4526887</li>
               </ul>
             </div>
             <div className="col l3 s12">
-              <h5 className="white-text quesha-font">Visítanos</h5>
+              <h5 className="white-text quesha-font center-align">Visítanos</h5>
               <ul>
-                <li><a className="white-text avenir-font" href="#!">Google API</a></li>
+                <li><a className="white-text avenir-font" href="#!"> <iframe src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.4370119817568!2d-75.55189728523031!3d6.337397995413764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442fcb0b153c59%3A0xf6e063556b7afcd6!2sLos+Dulces+De+Samy!5e0!3m2!1ses-419!2sco!4v1557170510285!5m2!1ses-419!2sco"} title={"Ubicación"}  width={"200"} height={"200"} frameborder={"0"}></iframe> </a></li>
               </ul>
             </div>
           </div>
